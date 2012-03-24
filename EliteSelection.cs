72329b0aa459591c619cfe04730b459d44cc120d
pozhidaevak <pozhidaevak@gmail.com>
2012-03-24 22:35:51 +0400

    Elite selection now works fine. Tornament selection added and tested

---
+++ EliteSelection.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
+namespace ga1
+{
+    public class EliteSelection<T> : ISelection<T>
+    {
+        public EliteSelection(int outLength, int eliteCount)
+        {
+            OutLength = outLength;
+            EliteCount = eliteCount;
+        }
+        public IChromosome<T>[] Select(IChromosome<T>[] population, Population<T>.Fitness fitness)
+        {
+            if (population.Length <= OutLength)
+            {
+                throw new ArgumentOutOfRangeException("Population to small");
+            }
+            List<IChromosome<T>> selectedChromosomes = new List<IChromosome<T>>(population.OrderByDescending(x => fitness(x)).Take(eliteCount));
+            List<IChromosome<T>> notEliteChromosomes = new List<IChromosome<T>>(population.OrderByDescending(x => fitness(x)).Skip(eliteCount));
+            for (int i = 0; i < outLength - eliteCount; ++i )
+            {
+                int rndInd = Program.rnd.Next(0, notEliteChromosomes.Count);
+                selectedChromosomes.Add(notEliteChromosomes[rndInd]);
+            }
+            Debug.Assert(selectedChromosomes.Count == outLength, "EliteSelection: selected wrong amount");
+            return selectedChromosomes.ToArray();
+        }
+        private int outLength, eliteCount;
+        public int OutLength
+        {
+            get
+            {
+                return outLength;
+            }
+            set
+            {
+                if (value < eliteCount || value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("outLength", outLength, " outLength must be bigger or equal to eliteCount");
+                }
+                else
+                {
+                    outLength = value;
+                }
+            }
+        }
+        public int EliteCount
+        {
+            get
+            {
+                return eliteCount;
+            }
+            set
+            {
+                if (value > outLength || value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("eliteCount", eliteCount, "eliteCount must be < outLength");
+                }
+                else
+                {
+                    eliteCount = value;
+                }
+            }
+        }
+    }
+}
---
+++ TornamentSelection.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ga1
+{
+    public class TornamentSelection<T> : ISelection<T>
+    {
+        public TornamentSelection(int outLength, int tornSize)
+        {
+            OutLength = outLength;
+            TornSize = tornSize;
+        }
+        public IChromosome<T>[] Select(IChromosome<T>[] population, Population<T>.Fitness fitness)
+        {
+            if (population.Length <= OutLength || population.Length <= tornSize * 2)
+            {
+                throw new ArgumentOutOfRangeException("Population to small");
+            }
+            IChromosome<T>[] outPopulation = new IChromosome<T>[outLength];
+            for (int i = 0; i < outPopulation.Length; ++i)
+            {
+                outPopulation[i] = population.OrderBy(x => Program.rnd.Next()).Take(tornSize).OrderByDescending(x => fitness(x)).Take(1).ToArray()[0];
+            }
+            return outPopulation;
+        }
+        private int outLength, tornSize;
+        public int OutLength
+        {
+            get
+            {
+                return outLength;
+            }
+            set
+            {
+                if ( value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("outLength", outLength, " outLength must be possitive");
+                }
+                else
+                {
+                    outLength = value;
+                }
+            }
+        }
+        public int TornSize
+        {
+            get
+            {
+                return tornSize;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("tornSize", tornSize, " tornSize must be possitive");
+                }
+                else
+                {
+                    tornSize = value;
+                }
+            }
+        }
+    }
+}
