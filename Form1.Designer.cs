0d5040ae34648031d32057e16a51a3bf80ec0fe7
pozhidaevak <pozhidaevak@gmail.com>
2012-04-24 11:58:13 +0400

    bug fix

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -80,7 +80,7 @@
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.Size = new System.Drawing.Size(346, 172);
             this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            
             // 
             // label1
             // 
